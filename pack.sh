a7946fcb111f55a7e8e3b9718081d19053f1d8bb
Mihai <141733462+MihaiCodrin@users.noreply.github.com>
2023-08-16 03:17:26 +0300

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -6,20 +6,15 @@ PROJECT_NAME=$(basename $(pwd))
 PACKAGE_CATALOG=${PROJECT_NAME}-${VERSION}
 JAR_NAME="${PACKAGE_CATALOG}.jar"
 
-# build project
 mvn clean package -Dmaven.test.skip=true
 
-# create tmp catalog
 mkdir ${PACKAGE_CATALOG}
 
-# files to package
 cp manifest.yml ${PACKAGE_CATALOG}
 cp --parents target/${JAR_NAME} ${PACKAGE_CATALOG}
 
-# prepare build manifest
 echo "commit_sha=$(git rev-parse HEAD)" > ${PACKAGE_CATALOG}/build_info.ini
 
-# create zip package
 cd ${PACKAGE_CATALOG}
 zip -r ../${PROJECT_NAME}-${VERSION}.zip *
 cd ..
